79e23b7b2c9f0f9a30360273a729969506a3abf5
0xFirekeeper <0xFirekeeper@gmail.com>
2024-10-24 18:07:57 +0700

    ver

--- Assets/Thirdweb/Runtime/Unity/ThirdwebManager.cs
+++ Assets/Thirdweb/Runtime/Unity/ThirdwebManager.cs
@@ -164,7 +164,7 @@ namespace Thirdweb.Unity
 
         public static ThirdwebManager Instance { get; private set; }
 
-        public static readonly string THIRDWEB_UNITY_SDK_VERSION = "5.5.5";
+        public static readonly string THIRDWEB_UNITY_SDK_VERSION = "5.6.0";
 
         private bool _initialized;
 
